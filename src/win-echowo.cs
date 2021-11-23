7b3de53a3aa0716dfea6761f457552cb8c8fa4aa
perceptified <perceptified@gmail.com>
2021-11-23 23:26:51 +0100

    Implemented UwU Interjections.

--- src/win-echowo.cs
+++ src/win-echowo.cs
@@ -5,9 +5,12 @@ namespace win_echowo
     // See https://aka.ms/new-console-template for more information
     public class win_echowo
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine(Replacements.UwuTransform(args[0]));
+            string inputText = args[0];
+            inputText = Replacements.UwuTransform(inputText);
+            inputText = Interjections.UwuInterject(inputText);
+            Console.WriteLine(inputText);
             // Console.WriteLine(Replacements.UwuTransform("ll orr or er na nan ma mam"));
         }
     }
---
+++ src/win-echowo/interjections.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace win_echowo
+{
+    public static class Interjections
+    {
+        static string[] UWUism = {
+                "uwu",
+                "owo",
+                "^w^",
+                "murr...",
+                "nya!",
+                "nyanyan!",
+                "huoooh...",
+                ":3",
+                "*wags tail*",
+                "nyea~",
+                "nyu...",
+                "rawr!"
+            };
+        public static string UwuInterject(string inputText)
+        {
+            Random rand = new Random();
+            int number;
+            List<int> indices = new List<int>();
+            int currentIdx = 0;
+            foreach (char c in inputText)
+            {
+                number = rand.Next(1, 11);
+                currentIdx = inputText.IndexOf(c, currentIdx);
+                if (c.ToString() == " ")
+                {
+                    if (number == 1)
+                    {
+                        inputText = inputText.Insert(inputText.IndexOf(c, currentIdx), " " + pickRandomUwuism());
+                    }
+                }
+                if (c.ToString() == Environment.NewLine)
+                {
+                    number = rand.Next(1, 6);
+                    if (number == 1)
+                    {
+                        inputText = inputText.Insert(inputText.IndexOf(c, currentIdx), " " + pickRandomUwuism());
+                    }
+                }
+            }
+            return inputText;
+        }
+        private static string pickRandomUwuism()
+        {
+            Random rando = new Random();
+            return UWUism[rando.Next(UWUism.Length)];
+        }
+    }
+}
